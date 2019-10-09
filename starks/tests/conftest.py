6cda5e453c65749d2aecdafdd768196ba3e93ea1
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2019-10-09 14:57:08 -0700

    changes

---
+++ starks/tests/conftest.py
@@ -0,0 +1,178 @@
+from functools import (
+    wraps,
+)
+import logging
+
+from eth_tester import (
+    EthereumTester,
+)
+import pytest
+from web3 import Web3
+from web3.providers.eth_tester import (
+    EthereumTesterProvider,
+)
+
+from starks import (
+    compile_lll,
+    compiler,
+    optimizer,
+)
+from starks.parser.parser_utils import (
+    LLLnode,
+)
+
+from .base_conftest import (
+    StarksContract,
+    _get_contract,
+    zero_gas_price_strategy,
+)
+
+# Import the base_conftest fixtures
+pytest_plugins = ['tests.base_conftest']
+
+############
+# PATCHING #
+############
+
+# setattr(eth_tester.backends.pyevm.main, 'GENESIS_GAS_LIMIT', 10**9)
+# setattr(eth_tester.backends.pyevm.main, 'GENESIS_DIFFICULTY', 1)
+
+
+def set_evm_verbose_logging():
+    logger = logging.getLogger('evm')
+    logger.setLevel('TRACE')
+
+
+# Useful options to comment out whilst working:
+# set_evm_verbose_logging()
+# from vdb import vdb
+# vdb.set_evm_opcode_debugger()
+
+@pytest.fixture
+def keccak():
+    return Web3.keccak
+
+
+@pytest.fixture
+def bytes_helper():
+    def bytes_helper(str, length):
+        return bytes(str, 'utf-8') + bytearray(length - len(str))
+    return bytes_helper
+
+
+@pytest.fixture
+def get_contract_from_lll(w3):
+    def lll_compiler(lll, *args, **kwargs):
+        lll = optimizer.optimize(LLLnode.from_list(lll))
+        bytecode, _ = compile_lll.assembly_to_evm(compile_lll.compile_to_assembly(lll))
+        abi = kwargs.get('abi') or []
+        c = w3.eth.contract(abi=abi, bytecode=bytecode)
+        deploy_transaction = c.constructor()
+        tx_hash = deploy_transaction.transact()
+        address = w3.eth.getTransactionReceipt(tx_hash)['contractAddress']
+        contract = w3.eth.contract(
+            address,
+            abi=abi,
+            bytecode=bytecode,
+            ContractFactoryClass=StarksContract,
+        )
+        return contract
+    return lll_compiler
+
+
+@pytest.fixture(scope='module')
+def get_contract_module():
+    tester = EthereumTester()
+    w3 = Web3(EthereumTesterProvider(tester))
+    w3.eth.setGasPriceStrategy(zero_gas_price_strategy)
+
+    def get_contract_module(source_code, *args, **kwargs):
+        return _get_contract(w3, source_code, *args, **kwargs)
+
+    return get_contract_module
+
+
+def get_compiler_gas_estimate(code, func):
+    if func:
+        return compiler.gas_estimate(code)[func] + 22000
+    else:
+        return sum(compiler.gas_estimate(code).values()) + 22000
+
+
+def check_gas_on_chain(w3, tester, code, func=None, res=None):
+    gas_estimate = get_compiler_gas_estimate(code, func)
+    gas_actual = tester.get_block_by_number('latest')['gas_used']
+    # Computed upper bound on the gas consumption should
+    # be greater than or equal to the amount of gas used
+    if gas_estimate < gas_actual:
+        raise Exception(f"Gas upper bound fail: bound {gas_estimate} actual {gas_actual}")
+
+    print(f'Function name: {func} - Gas estimate {gas_estimate}, Actual: {gas_actual}')
+
+
+def gas_estimation_decorator(w3, tester, fn, source_code, func):
+    def decorator(*args, **kwargs):
+        @wraps(fn)
+        def decorated_function(*args, **kwargs):
+            result = fn(*args, **kwargs)
+            if 'transact' in kwargs:
+                check_gas_on_chain(w3, tester, source_code, func, res=result)
+            return result
+        return decorated_function(*args, **kwargs)
+    return decorator
+
+
+def set_decorator_to_contract_function(w3, tester, contract, source_code, func):
+    func_definition = getattr(contract, func)
+    func_with_decorator = gas_estimation_decorator(
+        w3, tester, func_definition, source_code, func
+    )
+    setattr(contract, func, func_with_decorator)
+
+
+@pytest.fixture
+def get_contract_with_gas_estimation(tester, w3):
+    def get_contract_with_gas_estimation(source_code, *args, **kwargs):
+
+        contract = _get_contract(w3, source_code, *args, **kwargs)
+        for abi in contract._classic_contract.functions.abi:
+            if abi['type'] == 'function':
+                set_decorator_to_contract_function(
+                    w3, tester, contract, source_code, abi['name']
+                )
+        return contract
+    return get_contract_with_gas_estimation
+
+
+@pytest.fixture
+def get_contract_with_gas_estimation_for_constants(w3):
+    def get_contract_with_gas_estimation_for_constants(
+            source_code,
+            *args, **kwargs):
+        return _get_contract(w3, source_code, *args, **kwargs)
+    return get_contract_with_gas_estimation_for_constants
+
+
+@pytest.fixture
+def assert_compile_failed():
+    def assert_compile_failed(function_to_test, exception=Exception):
+        with pytest.raises(exception):
+            function_to_test()
+    return assert_compile_failed
+
+
+@pytest.fixture
+def search_for_sublist():
+
+    def search_for_sublist(lll, sublist):
+        _list = lll.to_list() if hasattr(lll, 'to_list') else lll
+        if _list == sublist:
+            return True
+        if isinstance(_list, list):
+            for i in _list:
+                ret = search_for_sublist(i, sublist)
+                if ret is True:
+                    return ret
+        return False
+
+    return search_for_sublist
--- starks/utils.py
+++ starks/utils.py
@@ -7,6 +7,314 @@ from starks.numbertype import Field
 from starks.numbertype import FieldElement
 from starks.poly_utils import multivariates_over
 
+import binascii
+from collections import (
+    OrderedDict,
+)
+import functools
+import re
+from typing import (
+    List,
+    Union,
+)
+
+from starks.exceptions import (
+    InvalidLiteralException,
+    VariableDeclarationException,
+)
+from starks.opcodes import (
+    opcodes,
+)
+
+try:
+    from Crypto.Hash import keccak
+    keccak256 = lambda x: keccak.new(digest_bits=256, data=x).digest()  # noqa: E731
+except ImportError:
+    import sha3 as _sha3
+    keccak256 = lambda x: _sha3.sha3_256(x).digest()  # noqa: E731
+
+
+# Converts four bytes to an integer
+def fourbytes_to_int(inp):
+    return (inp[0] << 24) + (inp[1] << 16) + (inp[2] << 8) + inp[3]
+
+
+# Converts string to bytes
+def string_to_bytes(str):
+    bytez = b''
+    for c in str:
+        if ord(c) >= 256:
+            raise InvalidLiteralException(f"Cannot insert special character {c} into byte array")
+        bytez += bytes([ord(c)])
+    bytez_length = len(bytez)
+    return bytez, bytez_length
+
+
+# Converts a provided hex string to an integer
+def hex_to_int(inp):
+    if inp[:2] == '0x':
+        inp = inp[2:]
+    return bytes_to_int(binascii.unhexlify(inp))
+
+
+# Converts bytes to an integer
+def bytes_to_int(bytez):
+    o = 0
+    for b in bytez:
+        o = o * 256 + b
+    return o
+
+
+# Encodes an address using ethereum's checksum scheme
+def checksum_encode(addr):  # Expects an input of the form 0x<40 hex chars>
+    assert addr[:2] == '0x' and len(addr) == 42
+    o = ''
+    v = bytes_to_int(keccak256(addr[2:].lower().encode('utf-8')))
+    for i, c in enumerate(addr[2:]):
+        if c in '0123456789':
+            o += c
+        else:
+            o += c.upper() if (v & (2**(255 - 4 * i))) else c.lower()
+    return '0x' + o
+
+
+# Returns lowest multiple of 32 >= the input
+def ceil32(x):
+    return x if x % 32 == 0 else x + 32 - (x % 32)
+
+
+# Calculates amount of gas needed for memory expansion
+def calc_mem_gas(memsize):
+    return (memsize // 32) * 3 + (memsize // 32) ** 2 // 512
+
+
+# Specific gas usage
+GAS_IDENTITY = 15
+GAS_IDENTITYWORD = 3
+
+# A decimal value can store multiples of 1/DECIMAL_DIVISOR
+DECIMAL_DIVISOR = 10000000000
+
+
+# Number of bytes in memory used for system purposes, not for variables
+class MemoryPositions:
+    ADDRSIZE = 32
+    MAXNUM = 64
+    MINNUM = 96
+    MAXDECIMAL = 128
+    MINDECIMAL = 160
+    FREE_VAR_SPACE = 192
+    FREE_VAR_SPACE2 = 224
+    BLANK_SPACE = 256
+    FREE_LOOP_INDEX = 288
+    RESERVED_MEMORY = 320
+
+
+# Sizes of different data types. Used to clamp types.
+class SizeLimits:
+    ADDRSIZE = 2**160
+    MAXNUM = 2**127 - 1
+    MINNUM = -2**127
+    MAXDECIMAL = (2**127 - 1) * DECIMAL_DIVISOR
+    MINDECIMAL = (-2**127) * DECIMAL_DIVISOR
+    MAX_UINT256 = 2**256 - 1
+
+    @classmethod
+    def in_bounds(cls, type_str, value):
+        assert isinstance(type_str, str)
+        if type_str == 'decimal':
+            return float(cls.MINDECIMAL) <= value <= float(cls.MAXDECIMAL)
+        if type_str == 'uint256':
+            return 0 <= value <= cls.MAX_UINT256
+        elif type_str == 'int128':
+            return cls.MINNUM <= value <= cls.MAXNUM
+        else:
+            raise Exception(f'Unknown type "{type_str}" supplied.')
+
+
+# Map representing all limits loaded into a contract as part of the initializer
+# code.
+LOADED_LIMIT_MAP = OrderedDict((
+    (MemoryPositions.ADDRSIZE, SizeLimits.ADDRSIZE),
+    (MemoryPositions.MAXNUM, SizeLimits.MAXNUM),
+    (MemoryPositions.MINNUM, SizeLimits.MINNUM),
+    (MemoryPositions.MAXDECIMAL, SizeLimits.MAXDECIMAL),
+    (MemoryPositions.MINDECIMAL, SizeLimits.MINDECIMAL),
+))
+
+
+RLP_DECODER_ADDRESS = hex_to_int('0x5185D17c44699cecC3133114F8df70753b856709')
+
+# Instructions for creating RLP decoder on other chains
+# First send 6270960000000000 wei to 0xd2c560282c9C02465C2dAcdEF3E859E730848761
+# Publish this tx to create the contract: 0xf90237808506fc23ac00830330888080b902246102128061000e60003961022056600060007f010000000000000000000000000000000000000000000000000000000000000060003504600060c082121515585760f882121561004d5760bf820336141558576001905061006e565b600181013560f783036020035260005160f6830301361415585760f6820390505b5b368112156101c2577f010000000000000000000000000000000000000000000000000000000000000081350483602086026040015260018501945060808112156100d55760018461044001526001828561046001376001820191506021840193506101bc565b60b881121561014357608081038461044001526080810360018301856104600137608181141561012e5760807f010000000000000000000000000000000000000000000000000000000000000060018401350412151558575b607f81038201915060608103840193506101bb565b60c08112156101b857600182013560b782036020035260005160388112157f010000000000000000000000000000000000000000000000000000000000000060018501350402155857808561044001528060b6838501038661046001378060b6830301830192506020810185019450506101ba565bfe5b5b5b5061006f565b601f841315155857602060208502016020810391505b6000821215156101fc578082604001510182826104400301526020820391506101d8565b808401610420528381018161044003f350505050505b6000f31b2d4f  # noqa: E501
+# This is the contract address: 0xCb969cAAad21A78a24083164ffa81604317Ab603
+
+# Available base types
+base_types = {'int128', 'decimal', 'bytes32', 'uint256', 'bool', 'address'}
+
+# Keywords available for ast.Call type
+valid_call_keywords = {'uint256', 'int128', 'decimal', 'address', 'contract', 'indexed'}
+
+# Valid base units
+valid_units = {'wei', 'sec'}
+
+# Valid attributes for global variables
+valid_global_keywords = {
+    'public',
+    'modifying',
+    'event',
+    'constant',
+} | valid_units | valid_call_keywords
+
+
+# Cannot be used for variable or member naming
+reserved_words = {
+    # types
+    'int128', 'uint256',
+    'address',
+    'bytes32',
+    'map',
+    'string', 'bytes',
+    # control flow
+    'if', 'for', 'while', 'until', 'pass',
+    'def',
+    # EVM operations and transaction properties
+    'push', 'dup', 'swap', 'send', 'call',
+    'selfdestruct', 'assert', 'stop', 'throw',
+    'raise', 'init', '_init_', '___init___', '____init____',
+    'msg',
+    # boolean literals
+    'true', 'false',
+    # more control flow and special operations
+    'self', 'this', 'continue',
+    # None sentinal value
+    'none',
+    # more special operations
+    'clear',
+    # denominations
+    'ether', 'wei', 'finney', 'szabo', 'shannon', 'lovelace', 'ada', 'babbage',
+    'gwei', 'kwei', 'mwei', 'twei', 'pwei',
+    # contract keywords
+    'contract', 'struct',
+    # units
+    'units',
+    # sentinal constant values
+    'zero_address', 'empty_bytes32' 'max_int128', 'min_int128', 'max_decimal',
+    'min_decimal', 'max_uint256',
+}
+
+# Otherwise reserved words that are whitelisted for function declarations
+function_whitelist = {
+    'send'
+}
+
+# List of valid LLL macros.
+valid_lll_macros = {
+    'assert', 'break', 'ceil32', 'clamp', 'clamp', 'clamp_nonzero', 'clampge',
+    'clampgt', 'clample', 'clamplt', 'codeload', 'continue', 'debugger', 'ge',
+    'if', 'le', 'lll', 'ne', 'pass', 'repeat', 'seq', 'set', 'sge', 'sha3_32',
+    'sha3_64', 'sle', 'uclampge', 'uclampgt', 'uclample', 'uclamplt', 'with',
+    '~codelen', 'label', 'goto'
+}
+
+
+# Is a variable or member variable name valid?
+# Same conditions apply for function names and events
+def is_varname_valid(varname, custom_units, custom_structs, constants):
+    from starks.functions import built_in_functions
+
+    varname_lower = varname.lower()
+    varname_upper = varname.upper()
+
+    if custom_units is None:
+        custom_units = set()
+    if varname_lower in {cu.lower() for cu in custom_units}:
+        return False, f"{varname} is a unit name."
+
+    # struct names are case sensitive.
+    if varname in custom_structs:
+        return False, f"Duplicate name: {varname}, previously defined as a struct."
+    if varname in constants:
+        return False, f"Duplicate name: {varname}, previously defined as a constant."
+    if varname_lower in base_types:
+        return False, f"{varname} name is a base type."
+    if varname_lower in valid_units:
+        return False, f"{varname} is a built in unit type."
+    if varname_lower in reserved_words:
+        return False, f"{varname} is a a reserved keyword."
+    if varname_upper in opcodes:
+        return False, f"{varname} is a reserved keyword (EVM opcode)."
+    if varname_lower in built_in_functions:
+        return False, f"{varname} is a built in function."
+    if not re.match('^[_a-zA-Z][a-zA-Z0-9_]*$', varname):
+        return False, f"{varname} contains invalid character(s)."
+
+    return True, ""
+
+
+def check_valid_varname(varname,
+                        custom_units,
+                        custom_structs,
+                        constants,
+                        pos,
+                        error_prefix="Variable name invalid.",
+                        exc=None):
+    """ Handle invalid variable names """
+    exc = VariableDeclarationException if exc is None else exc
+
+    valid_varname, msg = is_varname_valid(varname, custom_units, custom_structs, constants)
+    if not valid_varname:
+        raise exc(error_prefix + msg, pos)
+
+    return True
+
+
+def is_instances(instances, instance_type):
+    return all([isinstance(inst, instance_type) for inst in instances])
+
+
+def iterable_cast(cast_type):
+    def yf(func):
+        @functools.wraps(func)
+        def f(*args, **kwargs):
+            return cast_type(func(*args, **kwargs))
+        return f
+    return yf
+
+
+def indent(text: str, indent_chars: Union[str, List[str]] = ' ', level: int = 1) -> str:
+    """
+    Indent lines of text in the string ``text`` using the indentation
+    character(s) given in ``indent_chars`` ``level`` times.
+
+    :param text: A string containing the lines of text to be indented.
+    :param level: The number of times to indent lines in ``text``.
+    :param indent_chars: The characters to use for indentation.  If a string,
+        uses repetitions of that string for indentation.  If a list of strings,
+        uses repetitions of each string to indent each line.
+
+    :return: The indented text.
+    """
+    text_lines = text.splitlines(keepends=True)
+
+    if isinstance(indent_chars, str):
+        indented_lines = [indent_chars * level + line for line in text_lines]
+    elif isinstance(indent_chars, list):
+        if len(indent_chars) != len(text_lines):
+            raise ValueError('Must provide indentation chars for each line')
+
+        indented_lines = [
+            ind * level + line
+            for ind, line
+            in zip(indent_chars, text_lines)
+        ]
+    else:
+        raise ValueError('Unrecognized indentation characters value')
+
+    return ''.join(indented_lines)
+
+
 def annotate_source_code(
     source_code: str,
     lineno: int,
